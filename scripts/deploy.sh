8fcc2e2357fe725fa856c443c1450deceab0e5b3
xuxihai <xuxihai123@gmail.com>
2024-05-12 18:20:33 +0800

    update action4

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -10,7 +10,7 @@ chmod 600 "$SSH_PATH/deploy_key"
 
 sshpos="$SSH_USERNAME@$SSH_HOSTNAME:$SSH_RSYNC_DIR"
 
-rsync_e_args="ssh -i ${SSH_PATH}/deploy_key -o StrictHostKeyChecking=no"
+rsync_e_args="ssh -i $SSH_PATH/deploy_key -o StrictHostKeyChecking=no"
 
 # Do deployment
 cmd="rsync  -avzr --delete --exclude node_modules --exclude '.git*' -e '$rsync_e_args' '$GITHUB_WORKSPACE/*' '$sshpos'"
