01c1210dc016f77c757be8a73193081d8a0bc6b6
xuxihai <xuxihai123@gmail.com>
2024-05-12 18:07:41 +0800

    update action3

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -5,7 +5,7 @@ set -eu
 # Set deploy key
 SSH_PATH="$HOME/.ssh"
 mkdir -p "$SSH_PATH"
-echo "$SSH_PRIVATE_KEY" > "$SSH_PATH/deploy_key"
+echo -n "$SSH_PRIVATE_KEY" > "$SSH_PATH/deploy_key"
 chmod 600 "$SSH_PATH/deploy_key"
 
 sshpos="$SSH_USERNAME@$SSH_HOSTNAME:$SSH_RSYNC_DIR"
@@ -15,4 +15,5 @@ rsync_e_args="ssh -i ${SSH_PATH}/deploy_key -o StrictHostKeyChecking=no"
 # Do deployment
 cmd="rsync  -avzr --delete --exclude node_modules --exclude '.git*' -e '$rsync_e_args' '$GITHUB_WORKSPACE/*' '$sshpos'"
 echo "exec $cmd"
-`$cmd`
+result=$(eval "$cmd")
+echo "$result"
